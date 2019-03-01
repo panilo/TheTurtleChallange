c360996f9dcf8fa6345b1819a4ad7645839a1939
Danilo Desole <danilodesole@gmail.com>
2019-03-01 21:30:52 +0100

    First game logic added

---
+++ src/Entities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheTurtleChallange
+{
+    public enum Direction
+    {
+        North = 1, 
+        East = 2, 
+        South = ~North, 
+        West = ~East
+    }
+
+    public class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Position
+    {
+        public Tile Tile { get; set; }
+        public Direction Direction { get; set; }
+    }
+
+    public class GameSettings
+    {
+        public Position InitPosition { get; set; }
+        public Tile ExitPosition { get; set; }
+        public List<Tile> MinesPosition { get; set; }
+        public Tile GridSize { get; set; }
+
+        public void IsValid()
+        {
+            if (InitPosition == null)
+                throw new ArgumentNullException(nameof(InitPosition));
+            if (ExitPosition == null)
+                throw new ArgumentNullException(nameof(ExitPosition));
+            if (MinesPosition == null)
+                throw new ArgumentNullException(nameof(MinesPosition));
+            if (GridSize == null)
+                throw new ArgumentNullException(nameof(GridSize));
+        }
+    }
+
+
+}
---
+++ src/Logic.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheTurtleChallange
+{
+    public class Logic
+    {
+        private GameSettings _gameSettings;
+        private List<string> _moves;
+
+        public Logic(GameSettings gameSettings, List<string> moves)
+        {
+            this._gameSettings = gameSettings;
+            this._moves = moves;
+            PreinitChecks();
+        }
+
+        private void PreinitChecks()
+        {
+            if (_gameSettings == null)
+                throw new ArgumentNullException(nameof(_gameSettings));
+            if (_moves == null)
+                throw new ArgumentNullException(nameof(_moves));
+
+            _gameSettings.IsValid();            
+        }
+        
+        public void Play()
+        {
+            foreach(string move in _moves)
+            {
+                //Get next position
+                //Check what happen 
+            }
+        }
+
+        private Position GetNextPosition(Position lastPosition, string move)
+        {
+            Position nextPosition = new Position() { Tile = lastPosition.Tile, Direction = lastPosition.Direction };
+
+            switch (move)
+            {
+                case "m":
+                    //Get next tile
+                    nextPosition.Tile = GetNextTile(lastPosition.Tile, lastPosition.Direction);
+                    break;
+                case "r":
+                    //Get next facing
+                    nextPosition.Direction = GetNextDirection(lastPosition.Direction);
+                    break;
+                default:
+                    throw new InvalidOperationException("Not valid move");
+            }
+
+            return nextPosition;
+        }
+
+        private Direction GetNextDirection(Direction lastDirection)
+        {
+            //Using bitwise operation https://en.wikipedia.org/wiki/Bitwise_operations_in_C
+            return ~lastDirection;
+        }
+
+        private Tile GetNextTile(Tile lastTile, Direction lastDirection)
+        {
+            Tile nextTile = new Tile() { X = lastTile.X, Y = lastTile.Y };
+
+            if (lastDirection == Direction.North)
+            {                
+                nextTile.Y = lastTile.Y - 1;
+            }
+
+            if (lastDirection == Direction.South)
+            {                
+                nextTile.Y = lastTile.Y + 1;
+            }
+
+            if (lastDirection == Direction.East)
+            {
+                nextTile.X = lastTile.X + 1;             
+            }
+
+            if (lastDirection == Direction.West)
+            {
+                nextTile.X = lastTile.X - 1;
+            }
+
+            return nextTile;
+        }
+    }
+}
